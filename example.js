7208a7365cb42f045c06593f59aa0b5331783f14
Kasirajan S <kasirajan.s@optisolbusiness.com>
2022-10-04 12:28:21 +0530

    changes the value

--- example.js
+++ example.js
@@ -1,10 +1,9 @@
 function example()
 {
-    var a=10;
-    var b=20;
-    c=a+b;
+   var a=10;
+   var b=20;
+    var c=a+b;
     console.log("The value of a is"+a);
     console.log("The Example of GIT");
     console.log("The value is"+c);
-
-}
+}  
